1cded33258ea946ad82e0b6956785d060b3ab4e0
arifkhan101 <arifkhan230122@gmail.com>
2024-05-19 21:12:16 +0600

    created interface for student

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app: Application = express()
+const app: Application = express();
 
 // parser
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
 
-export default app
+export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,34 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContractNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContractNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth: string;
+  contractNor: string;
+  emergencyContractNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg: string;
+  isActive: 'active' | 'inActive';
+};
