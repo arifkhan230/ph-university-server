a64f0a1782584ae5f19f7fc10984526e4e1bfb4b
arifkhan101 <arifkhan230122@gmail.com>
2024-05-20 00:06:01 +0600

    created route for getting alll studentd

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
 
+// creating new student data
 const createStudent = async (req: Request, res: Response) => {
   try {
     const student = req.body.student;
     const result = await StudentServices.createStudentIntoDB(student);
 
-    console.log('its working');
     res.status(200).json({
       success: true,
       message: 'Student is created successfully',
@@ -17,6 +17,23 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+// getting all student data from database
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentController = {
   createStudent,
+  getAllStudents,
 };
