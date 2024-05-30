fc004ccda586d78d7412a7683c605aee663a56c2
arifkhan101 <arifkhan230122@gmail.com>
2024-05-30 10:09:09 +0600

    added logical validation in academic semester

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -2,8 +2,23 @@ import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
-  const result = await AcademicSemester.create(payload);
+  // semester --> semester code
 
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  };
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester code');
+  }
+
+  const result = await AcademicSemester.create(payload);
   return result;
 };
 
