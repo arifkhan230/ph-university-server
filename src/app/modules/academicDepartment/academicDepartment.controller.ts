37291ace192893cc264a497c281947692f3d604a
arifkhan101 <arifkhan230122@gmail.com>
2024-06-05 12:38:34 +0600

    completed academic department

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -54,7 +54,7 @@ const updateAcademicDepartment = catchAsync(async (req, res) => {
   });
 });
 
-export const AcademicFacultyControllers = {
+export const AcademicDepartmentControllers = {
   createAcademicDepartment,
   getAllAcademicDepartmentFromDB,
   getSingleAcademicDepartmentFromDB,
---
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartmentFromDB);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartmentFromDB,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
