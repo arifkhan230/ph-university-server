d7e82160076a67cabb51f8d8143869625d7fffaf
arifkhan101 <arifkhan230122@gmail.com>
2024-06-05 19:37:57 +0600

    added some validation on creating and updatind department

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,29 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// checking if the department already exists
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error('This department is already exist');
+  }
+  next();
+});
+
+// checking is department exist before updating
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exists!');
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
