348a4a06e4c9e50359891eb718732850bdd505fd
arifkhan101 <arifkhan230122@gmail.com>
2024-06-08 10:52:25 +0600

    handled mongose cast error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,6 +4,7 @@ import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import { TErrorSources } from '../interface/error';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
@@ -25,6 +26,12 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       (errorSources = simplifiedError?.errorSources);
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+
+    (statusCode = simplifiedError?.statusCode),
+      (message = simplifiedError?.message),
+      (errorSources = simplifiedError?.errorSources);
+  } else if (err.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
 
     (statusCode = simplifiedError?.statusCode),
       (message = simplifiedError?.message),
