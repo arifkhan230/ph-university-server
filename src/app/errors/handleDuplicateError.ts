d80c6e24e1bac62ff94efc422f0df50d35cc88ed
arifkhan101 <arifkhan230122@gmail.com>
2024-06-08 11:12:12 +0600

    handled duplicate error

---
+++ src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  const match = err.message.match(/"([^"]*)"/);
+
+  const extractedMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: `${extractedMessage} is already exist`,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'duplicate value error',
+    errorSources,
+  };
+};
+
+export default handleDuplicateError;
